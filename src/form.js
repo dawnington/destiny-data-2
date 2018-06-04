6f2c5fcefdf956e9a2d4b0f2e8137a5a8a598469
Dawn Tran <dawnington@gmail.com>
2018-06-03 21:56:18 -0700

    Add platform toggle

--- src/form.js
+++ src/form.js
@@ -14,11 +14,30 @@ class SearchForm extends React.Component {
     this.props.addPlayer(name, this.state.platform);
   }
 
+  handlePlatformChange = (platform) => (e) => {
+    e.preventDefault()
+    this.setState({ platform });
+  }
+
   render () {
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <fieldset disabled={this.props.disabled}>
+            <div className="btn-group btn-group-sm" role="group" aria-label="platform">
+              <button
+                className={`btn btn${this.state.platform === 1 ? '-outline' : ''}-primary btn-sm`}
+                onClick={this.handlePlatformChange(2)}
+              >
+                PS4
+              </button>
+              <button
+                className={`btn btn${this.state.platform === 2 ? '-outline' : ''}-primary btn-sm`}
+                onClick={this.handlePlatformChange(1)}
+              >
+                XBOX
+              </button>
+            </div>
             <label htmlFor="username">Username:
               <input
                 id="username"
@@ -27,7 +46,7 @@ class SearchForm extends React.Component {
                 ref={(input) => this.input = input}
               />
             </label>
-            <button className="btn btn-primary">Search</button>
+            <button className="btn btn-primary btn-sm">Search</button>
           </fieldset>
         </div>
       </form>
